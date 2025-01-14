1992dfa629f97ad19467c41b492e2a0884b27ddb
Nazril <73063248+Ryouiechziell@users.noreply.github.com>
2025-01-14 19:15:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.use(cors({origin: "*"}))
 app.use(express.static(__dirname + "/public"))
 
 app.get("/",(req,res) => {
-  res.sendFile("/yt.html")
+  res.sendFile(__dirname + "/public/yt.html")
 })
 
 app.listen(process.env.PORT || 5001)
