7df2124c1f064e9bc055a37a46440e6ad73076b4
Nazril <73063248+Ryouiechziell@users.noreply.github.com>
2025-01-14 16:31:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const cors = require("cors")
-
+const fs = require("fs")
+const yt = fs.readFileSync("./yt.html")
 const app = express()
 
 app.use(cors({origin: "*"}))
 app.use(express.json())
 
 app.get("/",(req,res) => {
-  res.sendFile("./yt.html")
+  res.sendFile(yt)
 })
 
 app.listen(process.env.PORT || 5001)
